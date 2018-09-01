c41b776388ab8cf35d9442e9ab1683edbf84eed2
Toshio Abe@pc01.local <abtoc88@gmail.com>
2018-09-02 01:48:27 +0900

    Bootstrapで装飾した

--- flaskr/views/__init__.py
+++ flaskr/views/__init__.py
@@ -1,6 +1,8 @@
-from flask import render_template
+from flask import render_template, flash
 from flaskr import app
 
 @app.route('/')
 def index():
+    flash('テストメッセージ', 'success')
+    flash('テストメッセージ', 'danger')
     return render_template('index.pug')
