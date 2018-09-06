a22b5178b50b5e0300426a860c73efc6f659a693
Toshio Abe@OfficeFarm <abtoc88@gmail.com>
2018-09-06 12:16:02 +0900

    トップ画面に３日分の勤怠データを表示させた

--- app/flaskr/views/__init__.py
+++ app/flaskr/views/__init__.py
@@ -1,14 +1,39 @@
-from flask import render_template
+from flask import render_template, url_for
 from datetime import date
 from dateutil.relativedelta import relativedelta
 from flaskr import app
-from flaskr.models import Person
+from flaskr.models import Person, WorkLog
+
+def _get_caption(person, yymmdd):
+    worklog = WorkLog.get_date(person.id, yymmdd)
+    yymm = yymmdd.strftime('%Y%m')
+    dd = yymmdd.day
+    if worklog is None:
+        caption = 'ー'
+        if person.staff:
+            url = url_for('worklogs.create', id=person.id, yymm=yymm, dd=dd)
+        else:
+            url = url_for('performlogs.create', id=person.id, yymm=yymm, dd=dd)
+    else:
+        if person.staff:
+            url = url_for('worklogs.edit', id=person.id, yymm=yymm, dd=dd)
+        else:
+            url = url_for('performlogs.edit', id=person.id, yymm=yymm, dd=dd)
+        if worklog.absence:
+            caption = '欠席'
+        elif bool(worklog.work_in) or bool(worklog.work_out):
+            caption = '{}-{}'.format(worklog.work_in, worklog.work_out)
+        else:
+            caption = 'ー'
+    return caption, url
 
 @app.route('/')
 def index():
     today = date.today()
-    prev = date.today() - relativedelta(months=1)
     yymm = today.strftime('%Y%m')
+    yesterday1 = date.today() - relativedelta(days=1)
+    yesterday2 = yesterday1 - relativedelta(days=1)
+    prev = date.today() - relativedelta(months=1)
     items = []
     persons = Person.query.filter(Person.enabled==True).order_by(Person.staff.asc(), Person.name.asc()).all()
     for person in persons:
@@ -17,7 +42,10 @@ def index():
         item['name'] = person.get_display()
         item['staff'] = person.staff
         item['yymm'] = yymm
-        item['yymm1'] = prev.strftime('%Y%m')
+        item['dd'] = today.day
+        item['caption'], item['url'] = _get_caption(person, today)
+        item['caption1'], item['url1'] = _get_caption(person, yesterday1)
+        item['caption2'], item['url2'] = _get_caption(person, yesterday2)
         items.append(item)
     return render_template('index.pug', items=items)
 
