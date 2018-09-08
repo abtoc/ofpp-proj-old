07f67ac13e453a17b23061327db90afa2ba0419d
Toshio Abe@pc01.local <abtoc88@gmail.com>
2018-09-08 13:55:53 +0900

    パスワード変更を実装した

--- app/flaskr/views/auth.py
+++ app/flaskr/views/auth.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template, redirect, request, url_for, flash
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, login_required, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo
 from flaskr import db, lm, cache
 from flaskr.models import User
 
@@ -15,6 +15,13 @@ def load_user(id):
 class LoginForm(FlaskForm):
     userid = StringField('ユーザID')
     password = PasswordField('パスワード')
+
+class PasswordChangeForm(FlaskForm):
+    password = PasswordField('パスワード', validators=[
+            DataRequired(message='必須入力です'),
+            EqualTo('confirm', message='パスワードが一致しません')
+        ])
+    confirm = PasswordField('パスワード再入力')
 
 @bp.route('/login',methods=['GET','POST'])
 def login():
@@ -35,3 +42,20 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+
+@bp.route('/passwd',methods=['GET','POST'])
+@login_required
+def passwd():
+    form = PasswordChangeForm()
+    if form.validate_on_submit():
+        user = current_user
+        user.set_password(form.password.data)
+        db.session.add(user)
+        try:
+            db.session.commit()
+            flash('パスワードの変更が完了しました', 'success')
+            return redirect(url_for('index'))
+        except Exception as e:
+            db.session.rollback()
+            flash('パスワードの変更が失敗しました "{}"'.format(e), 'danger')
+    return render_template('auth/edit.pug', form=form)
